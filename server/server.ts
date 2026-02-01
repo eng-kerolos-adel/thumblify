7d2a563582183ce44086f1273f47726e13968161
eng-kerolos-adel <eng.kerolos.adel.eleshaa@gmail.com>
2026-02-01 15:01:23 +0200

    first commit

--- server/server.ts
+++ server/server.ts
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["https://thumblify-client-mu.vercel.app", "https://thumblify-server-chi.vercel.app"],
     credentials: true,
   }),
 );
